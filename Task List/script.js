c8bdb715d813f750899e34d0d5ad4fad1f39d300
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2024-07-15 16:39:10 +0600

    updated Task List: added add task and remove task

--- Task List/script.js
+++ Task List/script.js
@@ -10,10 +10,23 @@ document
 
 function addTask() {
   let task_title = document.getElementById("task-input").value;
-  taskList.push(task_title);
+
+  //find an empty spot in task array and insert
+  let inserted = false;
+  let i = 0;
+  for (i = 0; i < taskList.length; i++) {
+    if (taskList[i] == "") {
+      inserted = true;
+      taskList[i] = task_title;
+    }
+  }
+  if (!inserted) taskList.push(task_title);
+  //find an empty spot in task array and insert
+
   let task_list_ol = document.getElementById("task-list-ol");
   let task_list_li = document.createElement("li");
-  task_list_li.innerHTML = `${task_title}`;
+  task_list_li.id = `task-li-${i}`;
+  task_list_li.innerHTML = `${task_title} <button onclick = "removeTask(${i})">Remove task</button>`;
   task_list_ol.appendChild(task_list_li);
 }
 function filterTask() {
@@ -21,4 +34,10 @@ function filterTask() {
 }
 function clearTask() {
   console.log("clear-task?");
+}
+
+function removeTask(taskNumber) {
+  let list_item = document.getElementById(`task-li-${taskNumber}`);
+  taskList[taskNumber] = "";
+  list_item.parentNode.removeChild(list_item);
 }
